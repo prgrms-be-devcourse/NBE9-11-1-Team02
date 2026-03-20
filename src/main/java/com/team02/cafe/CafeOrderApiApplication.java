49b4ce453038a982a5364f60aecb0cc4a69c47a6
hyhdb <kkminwoo@gmail.com>
2026-03-20 10:13:58 +0900

    init: project setup

---
+++ src/main/java/com/team02/cafe/CafeOrderApiApplication.java
@@ -0,0 +1,13 @@
+package com.team02.cafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeOrderApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CafeOrderApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/team02/cafe/CafeOrderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team02.cafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeOrderApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
