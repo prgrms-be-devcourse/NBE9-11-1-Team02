d6479af480c17e4dbc99db540e30570b43d2328a
minho <minho01@hs.ac.kr>
2026-03-20 16:49:13 +0900

    merge를 위한 중간 commit

--- backend/src/main/java/com/team02/cafe/domain/order/repository/OrderRepository.java
+++ backend/src/main/java/com/team02/cafe/domain/order/repository/OrderRepository.java
@@ -8,7 +8,7 @@ import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Repository
-public interface OrderRepository  {
+public interface OrderRepository extends JpaRepository<Order, Long> {
     // 이메일과 시간 범위(전날 오후 2시 ~ 당일 오후 2시)를 기준으로 기존 주문 검색
     Optional<Order> findByEmailAndCreatedAtBetween(String email, LocalDateTime start, LocalDateTime end);
 }
