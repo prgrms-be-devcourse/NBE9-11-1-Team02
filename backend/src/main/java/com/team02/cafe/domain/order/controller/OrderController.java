da3dea63750cf4ba2ea2f83a6528a1ba6683fc3e
minho <minho01@hs.ac.kr>
2026-03-20 17:38:26 +0900

    주문 목록과 주문 상세를 조회하는 API 추가

--- backend/src/main/java/com/team02/cafe/domain/order/controller/OrderController.java
+++ backend/src/main/java/com/team02/cafe/domain/order/controller/OrderController.java
@@ -1,9 +1,14 @@
 package com.team02.cafe.domain.order.controller;
 
+import com.team02.cafe.domain.order.dto.OrderDetailResponseDto;
 import com.team02.cafe.domain.order.dto.OrderRequest;
+import com.team02.cafe.domain.order.dto.OrderResponseDto;
 import com.team02.cafe.domain.order.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/orders")
@@ -23,5 +28,16 @@ public class OrderController {
     @PatchMapping("/{orderId}/cancel")
     public void cancelOrder(@PathVariable Long orderId) {
         orderService.cancelOrder(orderId);
+    }
+    // [추가] 주문 목록 조회 API
+    @GetMapping
+    public ResponseEntity<List<OrderResponseDto>> getOrders() {
+        return ResponseEntity.ok(orderService.getAllOrders());
+    }
+
+    // [추가] 주문 상세 조회 API
+    @GetMapping("/{orderId}")
+    public ResponseEntity<OrderDetailResponseDto> getOrderDetails(@PathVariable Long orderId) {
+        return ResponseEntity.ok(orderService.getOrderDetails(orderId));
     }
 }
