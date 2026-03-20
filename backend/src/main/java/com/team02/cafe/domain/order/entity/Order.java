08e22939c7a235d6aa5145682c068d2afce2d707
hyhdb <kkminwoo@gmail.com>
2026-03-20 11:39:46 +0900

    core setting

---
+++ backend/src/main/java/com/team02/cafe/domain/order/entity/Order.java
@@ -0,0 +1,43 @@
+package com.team02.cafe.domain.order.entity;
+
+import com.team02.cafe.global.common.BaseTimeEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "orders")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Order extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private String address;
+
+    private String phoneNumber;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+
+    private Long totalPrice;
+
+    public Order(String email, String address, String phoneNumber, OrderStatus orderStatus, Long totalPrice) {
+        this.email = email;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.orderStatus = orderStatus;
+        this.totalPrice = totalPrice;
+    }
+}
