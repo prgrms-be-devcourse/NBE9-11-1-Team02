18fb614f5cba54f5621ca4c26c02e43ddbb102d3
hyhdb <kkminwoo@gmail.com>
2026-03-20 11:20:17 +0900

    chore: split repository into backend and frontend

--- backend/src/main/java/com/team02/cafe/CafeOrderApiApplication.java
+++ backend/src/main/java/com/team02/cafe/CafeOrderApiApplication.java
@@ -2,12 +2,13 @@ package com.team02.cafe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class CafeOrderApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CafeOrderApiApplication.class, args);
     }
-
 }
---
+++ backend/src/main/java/com/team02/cafe/domain/product/entity/Product.java
@@ -0,0 +1,4 @@
+package com.team02.cafe.domain.product.entity;
+
+public class Product {
+}
