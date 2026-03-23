13b0b090bfa11e85b2370f03ded3e90baa85931e
minho <116282998+minho01@users.noreply.github.com>
2026-03-23 10:03:43 +0900

    Feature/get order list (#19)
    * feat: 주문 및 주문 상품 조회를 위한 Repository 추가
    
    * feat: 주문 목록 및 상세 조회용 DTO 클래스 생성
    
    * extend JpaRepository 누락으로 인한 오류 수정
    
    * merge를 위한 중간 commit
    
    * 이메일/시간 기준 주문을 찾아 í합치는 기ë능고 목록능과¸ 조회 기능 추가가
    
    * 주문 목록과 주문 상세를 조회하는 API 추가

--- backend/src/main/java/com/team02/cafe/domain/order/repository/OrderRepository.java
+++ backend/src/main/java/com/team02/cafe/domain/order/repository/OrderRepository.java
@@ -3,10 +3,12 @@ package com.team02.cafe.domain.order.repository;
 import com.team02.cafe.domain.order.entity.Order;
 import com.team02.cafe.domain.order.entity.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
 
+@Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
     // 같은 이메일 + 같은 주문 집계 시간대 + 취소되지 않은 주문 조회
