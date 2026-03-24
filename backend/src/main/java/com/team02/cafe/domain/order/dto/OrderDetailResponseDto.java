38fb36933dc58d0c96beb136524171bc20c013b9
minho <minho01@hs.ac.kr>
2026-03-24 15:02:28 +0900

    fix: 주문 조회 시 상품 정보 오류 수정

--- backend/src/main/java/com/team02/cafe/domain/order/dto/OrderDetailResponseDto.java
+++ backend/src/main/java/com/team02/cafe/domain/order/dto/OrderDetailResponseDto.java
@@ -10,13 +10,7 @@ import java.util.stream.Collectors;
 // OrderResponseDto를 상속받아 공통 필드(이메일, 주소 등)를 가져옵니다.
 public class OrderDetailResponseDto extends OrderResponseDto {
 
-    private List<OrderProductDto> orderProducts;
-
     public OrderDetailResponseDto(Order order) {
-        super(order); // 부모 클래스 생성자 호출
-        // Order 엔티티에 양방향 연관관계가 있으므로 바로 꺼내어 변환합니다.
-        this.orderProducts = order.getOrderProducts().stream()
-                .map(OrderProductDto::new)
-                .collect(Collectors.toList());
+        super(order);
     }
 }
