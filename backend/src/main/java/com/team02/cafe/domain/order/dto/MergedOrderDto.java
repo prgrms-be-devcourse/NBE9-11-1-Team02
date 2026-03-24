7be932484b18ab61829b6faa588f4f61e27ac823
minho <minho01@hs.ac.kr>
2026-03-24 21:29:19 +0900

    feat: 주문 번호(UUID) 프론트엔드 반환 로직 추가 (DTO 및 Service 수정)

--- backend/src/main/java/com/team02/cafe/domain/order/dto/MergedOrderDto.java
+++ backend/src/main/java/com/team02/cafe/domain/order/dto/MergedOrderDto.java
@@ -11,6 +11,7 @@ import java.util.List;
 @AllArgsConstructor
 public class MergedOrderDto {
     private Long orderId;
+    private String orderNumber;
     private String email;
     private String username;
     private String address;
